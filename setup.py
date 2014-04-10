358557c02a7e06e967847254073d731835cdd6a7
David Cournapeau <cournape@gmail.com>
2014-04-10 13:47:17 +0100

    MAINT: add tox and basic distutils script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(name="sqlite_cache",
+          version="0.0.1",
+          author="David Cournapeau",
+          packages=["sqlite_cache", "sqlite_cache.tests"],
+          license="BSD",
+         )
