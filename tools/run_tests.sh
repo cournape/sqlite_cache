4fc1afe8ffacbaafdf1acf5de6e85fcab6655a99
David Cournapeau <cournape@gmail.com>
2014-04-10 18:53:25 +0100

    MAINT: tweak run tests script to get actual coverage in coveralls.

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 set -e
 coverage erase
-coverage run --branch -m haas  sqlite_cache
+coverage run --branch --source=sqlite_cache -m haas  sqlite_cache
 coverage report --include=sqlite_cache/* -m
