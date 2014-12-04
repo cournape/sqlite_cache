7712c410c4e971b23e847468ab6e043bdc9d4086
David Cournapeau <cournape@gmail.com>
2014-12-04 16:24:01 +0000

    MAINT: start 0.0.3.

--- setup.py
+++ setup.py
@@ -2,8 +2,7 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(name="sqlite_cache",
-          version="0.0.2",
+          version="0.0.3.dev1",
           author="David Cournapeau",
           packages=["sqlite_cache", "sqlite_cache.tests"],
-          license="BSD",
-         )
+          license="BSD")
